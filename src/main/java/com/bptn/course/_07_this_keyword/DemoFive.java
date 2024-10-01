f5b304ac0b426829a7aa3fdc9c7ce7bfd59fa75c
Umukulthum <kanyinsolaumukulthum@gmail.com>
2024-10-01 15:45:17 -0400

    this keyword concept implementation

---
+++ src/main/java/com/bptn/course/_07_this_keyword/DemoFive.java
@@ -0,0 +1,26 @@
+package com.bptn.course._07_this_keyword;
+
+public class DemoFive {
+	public static void main(String[] args) {
+		OuterClass outer = new OuterClass();
+		OuterClass.InnerClass inner = outer.new InnerClass();
+		inner.displayFields();
+		// Output:
+		// Inner Field: Inner Class Field
+		// Outer Field: Outer Class Field
+	}
+
+}
+
+class OuterClass {
+	private String outerField = "Outer Class Field";
+
+	class InnerClass {
+		private String innerField = "Inner Class Field";
+
+		public void displayFields() {
+			System.out.println("Inner Field: " + this.innerField); // Refers to InnerClass' field
+			System.out.println("Outer Field: " + OuterClass.this.outerField); // Refers to OuterClass' field
+		}
+	}
+}
---
+++ src/main/java/com/bptn/course/_07_this_keyword/DemoTwo.java
@@ -0,0 +1,5 @@
+package com.bptn.course._07_this_keyword;
+
+public class DemoTwo {
+
+}
